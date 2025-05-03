4c6d48f5f09130102385e2ef930c462ffd09e3e9
Breno Lima <breno.lima@mcsmarkup.com.br>
2025-05-03 01:57:19 -0300

    final fix for deployment

--- main.py
+++ main.py
@@ -3,19 +3,15 @@ import requests
 
 app = FastAPI()
 
-@app.get()
-def root()
-    return {status Jade Atlas backend ativo}
+@app.get("/")
+def root():
+    return {"status": "Jade Atlas backend ativo"}
 
-@app.post(notify)
-def send_notification(message str = Sistema Jade Atlas ativado. Comunicação operacional.)
-    telegram_token = 6521799621AAEfqE6Tn1z4hHRfDAzPQaRz4Swih9D0wms
-    chat_id = 5913942179
-    telegram_url = fhttpsapi.telegram.orgbot{telegram_token}sendMessage
-
-    payload = {chat_id chat_id, text message}
+@app.post("/notify")
+def send_notification(message: str = "Sistema Jade Atlas ativado. Comunicação operacional."):
+    telegram_token = "6746078990:AAE0Fc4-pcmDboC0e_xF4Zq1IvZ-DxlDhaU"
+    chat_id = "5070282354"
+    telegram_url = f"https://api.telegram.org/bot{telegram_token}/sendMessage"
+    payload = {"chat_id": chat_id, "text": message}
     response = requests.post(telegram_url, json=payload)
-    return {
-        status mensagem enviada,
-        telegram_response response.json()
-    }
+    return {"status": "mensagem enviada", "telegram_response": response.json()}
